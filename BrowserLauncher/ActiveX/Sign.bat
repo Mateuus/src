7466379394d485878f45410b1f7a0e58483ee726
Mateuus <muvucasbars@outlook.com>
2014-09-17 17:31:01 -0300

    Delete BrowserLauncher -pra fica mais leve

--- BrowserLauncher/ActiveX/Sign.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set CAB=WarIncLaunch_1_0_0_9.cab
-signtool.exe sign /a /t http://timestamp.verisign.com/scripts/timestamp.dll /v release/WarIncLaunch.dll
-makecab /f WarIncLaunch.ddf
-signtool.exe sign /a /t http://timestamp.verisign.com/scripts/timestamp.dll /d "War Inc. Battle Zone Launcher" /v %CAB%
-del setup.rpt
-del setup.inf
--- BrowserLauncher/NPAPI/pack.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd xpi
-set XPI=npwiu_1_0_0_3.xpi
-:start /wait "C:\Program Files\WinRAR\winrar" "a -r -x.svn -xplatform\.svn -xplatform\WINNT_x86-msvc\.svn -xplatform\WINNT_x86-msvc\plugins\.svn ..\%XPI%.zip
-pkzipc -add -dir -excl=.svn ..\%XPI%.zip
-cd ..
-del %XPI% 2> nul
-ren %XPI%.zip %XPI%
